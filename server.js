fc5c5ca657dfa0336347a82a8e92fd0faac5d433
cbmartinez42 <cbmartinez42@gmail.com>
2021-08-12 17:06:57 -0500

    updating db url

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get("/", function(req, res) {
 });
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dothisdb", { 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/apex-stats", { 
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
